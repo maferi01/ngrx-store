dfe2c4376fd95bc0a85cba76e1cee3ef3447ca7b
INECO\arturo.iniesta <a.fernandezin@gmail.com>
2022-02-04 12:41:52 +0100

    working storis  witj pack5

--- src/app/shared/base/abstract-component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractComponent } from './abstract-component';
-
-describe('AbstractComponent', () => {
-  it('should create an instance', () => {
-    expect(new AbstractComponent()).toBeTruthy();
-  });
-});
