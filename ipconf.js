24a5c002041db4bb8c3f0fa4f5c77c86a5b38ffe
maferi01 <a.fernandezin@gmail.com>
2022-03-09 11:53:04 +0100

    ip public

--- ipconf.js
+++ ipconf.js
@@ -2,6 +2,15 @@
 
 const { networkInterfaces } = require('os');
 
+
+import publicIp from 'public-ip';
+
+console.log('ip v4' ,await publicIp.v4());
+//=> '46.5.21.123'
+
+console.log('ip v6' ,await publicIp.v6());
+//=> 'fe80::200:f8ff:fe21:67cf'
+
 const nets = networkInterfaces();
 const results = Object.create(null); // Or just '{}', an empty object
 
